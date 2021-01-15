ae4607f87addb3e121f07b29d575af8cfa60fba4
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2021-01-14 23:42:23 -0500

    atlas heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const logger = require("morgan");
-const db = "workouts_db";
+const db = "Workouts";
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
